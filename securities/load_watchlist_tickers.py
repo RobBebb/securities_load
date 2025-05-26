10e5835db0908049e1ea7e9399b586dbd02620c9
Rob Bebbington <rob.bebbington@outlook.com>
2025-05-27 08:14:16 +1000

    Updated load_warchlist_tickers

--- securities/load_watchlist_tickers.py
+++ securities/load_watchlist_tickers.py
@@ -12,33 +12,32 @@ from securities_load.securities.securities_table_functions import (
     add_or_update_watchlist_tickers,
 )
 
-module_logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 
 def load_watchlist_tickers(watchlist_csv_file: str) -> None:
     """
     Get the tickers and load them into the equity ticker table"""
 
-    module_logger.debug("Started")
+    logger.debug("Started")
 
     # load_dotenv()
     # Open a connection
     conn = connect()
     engine = sqlalchemy_engine()
+
     watchlist_tickers = read_watchlist_tickers(watchlist_csv_file)
 
-    # Write the data to the local polygon database
-    # if ticker_list is not None:
-    #     add_tickers(conn, ticker_list)
-    # print(watchlist_tickers.head())
+    # Write the data to the securities schema
     watchlist_tickers_transformed = transform_watchlist_tickers(
         engine, watchlist_tickers
     )
-    # print(watchlist_tickers_transformed.head())
-    module_logger.debug("Transformed")
+
+    logger.debug("Transformed")
 
     if watchlist_tickers_transformed is not None:
         add_or_update_watchlist_tickers(conn, watchlist_tickers_transformed)
 
     # Close the connection
     conn.close()
+    logger.debug("Finished")
--- securities/run_load_watchlist_tickers.py
+++ securities/run_load_watchlist_tickers.py
@@ -1,7 +1,30 @@
+from securities_load.securities.load_watchlist_tickers import load_watchlist_tickers
+
+load_watchlist_tickers("/home/ubuntuuser/karra/securities_load/data/watchlists.csv")
+
+
+import logging
+
 from dotenv import load_dotenv
 
 from securities_load.securities.load_watchlist_tickers import load_watchlist_tickers
 
 load_dotenv()
 
+
+logger = logging.getLogger(__name__)
+
+logging.basicConfig(
+    filename="load_watchlist_tickers.log",
+    filemode="w",
+    encoding="utf-8",
+    level=logging.INFO,
+    format="{asctime} - {name}.{funcName} - {levelname} - {message}",
+    style="{",
+)
+
+logger.info("Start")
+
 load_watchlist_tickers("/home/ubuntuuser/karra/securities_load/data/watchlists.csv")
+
+logger.info("Finish")
